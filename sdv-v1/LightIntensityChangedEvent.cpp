9136055ef491163f86bfb8d2e450a9a52057035c
Seonghyeon Shin <sin6708k@gmail.com>
2026-08-20 12:34:01 +0900

    feat: 터널 진입 시나리오와 관련 구현체 추가
    - 관련 구현체: 조도 센서(`LightSensor`), 전조등(`Headlight`), 그리고 이를 컨트롤하는 컨트롤러(`HeadlightController`)

---
+++ sdv-v1/LightIntensityChangedEvent.cpp
@@ -0,0 +1,6 @@
+#include "LightIntensityChangedEvent.h"
+
+LightIntensityChangedEvent::LightIntensityChangedEvent(double light_intensity) :
+    light_intensity(light_intensity)
+{
+}
---
+++ sdv-v1/LightSensor.cpp
@@ -0,0 +1,23 @@
+#include "LightSensor.h"
+
+LightSensor::LightSensor() :
+    light_intensity_(0.0),
+    environment_(nullptr)
+{
+}
+
+void LightSensor::set_environment(IEnvironment* environment)
+{
+    environment_ = environment;
+}
+
+void LightSensor::sampling()
+{
+    if (environment_ == nullptr)
+    {
+        return;
+    }
+
+    light_intensity_ = std::stod(environment_->observe("light_intensity").value);
+    emit(LightIntensityChangedEvent(light_intensity_));
+}
